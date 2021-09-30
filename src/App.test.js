0e638043a2345ac87048ce4a7f94f5a95ac6fe64
JC <james.calingo@gmail.com>
2021-09-30 11:37:17 -0400

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders tagline', () => {
-  render(<App />);
-  const tagline = screen.getByText(/technology is the light that can cut through the darkness/i);
-  expect(tagline).toBeInTheDocument();
-});
