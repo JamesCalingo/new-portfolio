9f7a16a7e776dd02339bf1e135033811afdd22a0
JC <james.calingo@gmail.com>
2021-11-05 11:14:54 -0400

    Updates

---
+++ precache-manifest.18b0b054f3bf6d29b0bb2d2c2bdf950e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b624925757db707a266d0291bc8d97e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5326425f54d584e3b4e4",
+    "url": "/static/css/main.902d318a.chunk.css"
+  },
+  {
+    "revision": "c417ec3d316055d288dd",
+    "url": "/static/js/2.464fade8.chunk.js"
+  },
+  {
+    "revision": "1945100ef61edd69928bbfbef5e62b8d",
+    "url": "/static/js/2.464fade8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b13b4cbd30a509a81c",
+    "url": "/static/js/3.65497e81.chunk.js"
+  },
+  {
+    "revision": "5326425f54d584e3b4e4",
+    "url": "/static/js/main.a6c0cb4a.chunk.js"
+  },
+  {
+    "revision": "3ef405d17b4da1b39ad5",
+    "url": "/static/js/runtime-main.714f2e35.js"
+  },
+  {
+    "revision": "eed135a16577c80f3f4f82b7ed58419d",
+    "url": "/static/media/Gmail_2013.eed135a1.png"
+  },
+  {
+    "revision": "40b4c410ea3848e3dda3d310a3f5b6e6",
+    "url": "/static/media/picture-of-me.40b4c410.jpg"
+  }
+]);
