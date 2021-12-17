b13cfd76a73db91a527e152363259bd9c1993a51
JC <james.calingo@gmail.com>
2021-12-17 11:34:22 -0500

    changed mobile blog link

--- src/components/pages/MobileMenu.js
+++ src/components/pages/MobileMenu.js
@@ -19,7 +19,7 @@ export default function MobileMenu() {
             <a href="#contact">Contact Me</a>
           </li> */}
           <li className="nav-item">
-            <a href="https://jamescalingo.com/">Blog</a>
+            <a href="https://overclocked-overthoughts.netlify.app">Blog</a>
           </li>
           <li className="nav-item">
             <a href="https://docs.google.com/document/d/e/2PACX-1vQcFopUsFf6TglXF6i6vTRadVZbpgs1bSS-UJ6w07hVXV1EbfkUrr5YeeMcs5tXTZwYnGcqqtQZguHk/pub">
